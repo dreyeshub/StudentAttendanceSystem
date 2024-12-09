4b96f741b6a5e7898d30c77cddd53ba64ca9676c
reyes.denice.catalla@gmail.com <reyes.denice.catalla@gmail.com>
2024-12-10 01:34:41 +0800

    final project

---
+++ pages/home.php
@@ -0,0 +1,24 @@
+<div class="page-title"> Attendance Management System </div>
+
+<hr>
+
+
+<head>
+ 
+    <style>
+        body {
+            text-align: center; 
+            background-color: #f0f0f0; 
+        }
+
+        h1 {
+            color: black;
+            margin-top: 150px; 
+            font-size: 3.0em; 
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to the Student Attendance System!</h1>
+</body>
+</html>
